d1e62cee51b4cb5e17539ebc574709d6b566b9ff
margl13 <62597926+margl13@users.noreply.github.com>
2020-11-04 08:55:53 +0100

    hw vue 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+   <div id="todo-list">
+      <input
+         v-model="newTodoText"
+         v-on:keyup.enter="addNewTodo"
+         placeholder="Add todo"
+      >
+      <button @click="addNewTodo">Add</button>
+      <ul>
+         <li
+                 is="todo-item"
+                 v-for="(todo, i) in todos"
+                 v-bind:key="todo.id"
+                 v-bind:title="todo.text"
+                 v-on:remove="todos.splice(i, 1)"
+         >
+            {{ todo.text }}
+         <button @click="deleteItem(i)">Remove</button>
+         </li>
+      </ul>
+   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+   data() {
+     return{
+        newTodoText: '',
+        todos: [
+           { id: 1, text: 'Wake up' },
+           { id: 2, text: 'Take a shower' },
+           { id: 3, text: 'Make a latte' },
+           { id: 4, text: 'Dress up' }
+        ],
+        nextTodoId: 5
+     }
+   },
+
+   methods: {
+     addNewTodo: function () {
+      this.todos.push({
+         id: this.nextTodoId++,
+         text: this.newTodoText
+      })
+        this.newTodoText = ''
+     },
+      deleteItem(i) {
+        this.todos.splice(i, 1);
+        this.count--;
+      }
+     }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
